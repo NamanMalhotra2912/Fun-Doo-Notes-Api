44554fb04fa9bdcefc0d2280d7bd3049b2ac02f4
NamanMalhotra2912 <neerajmalhotra0001@gmail.com>
2021-05-12 19:12:46 +0530

    Updated database config file

--- server.js
+++ server.js
@@ -1,32 +1,23 @@
+// Express.js is a back end web application framework for Node.js which can be used for writing business logic.
 const express = require('express');
+const MongoDBAdapter = require('./config/databaseConfig')
+
+const port = process.env.PORT;
+// const host = process.env.HOST;
+
 const bodyParser = require('body-parser');
 
 // create express app
 const app = express();
 
 // parse requests of content-type - application/x-www-form-urlencoded
+//use is a way to register middleware or chain of middlewares before executing any end route logic.
 app.use(express.json());
 
 // parse requests of content-type - application/json
 // app.use(bodyParser.json())
 app.use(express.json());
-// Configuring the database
-const dbConfig = require('./config/database.config.js');
-const mongoose = require('mongoose');
 
-mongoose.Promise = global.Promise;
-
-// Connecting to the database
-mongoose.connect(dbConfig.url, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true
-}).then(() => {
-    console.log("Successfully connected to the database");    
-}).catch(err => {
-    console.log('Could not connect to the database. Exiting now...', err);
-    process.exit();
-});
 // define a simple route
 app.get('/', (req, res) => {
     res.json({"message": "Welcome to Fun Doo Notes application. Take notes quickly. Organize and keep track of all your notes."});
@@ -36,7 +27,26 @@ app.get('/', (req, res) => {
 require('./app/routes/user.js')(app);
 
 // listen for requests
-app.listen(3000, () => {
-    console.log("Server is listening on port 3000");
-});
+// app.listen(3000, async() => {
+//     console.log("Server is listening on port 3000");
+// });
+app.listen(port, async () => {
+    var db = new MongoDBAdapter(process.env.DB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    });
+    // console.log(db);
+    await db
+      .connect()
+      .then((uri) => {
+          console.log(uri);
+        console.log('Connected to ' + uri);
+      })
+      .catch((uri) => {
+        db.disconnect();
+        console.log('Disconnected from ' + uri);
+      });
+    });
 module.exports = app;
