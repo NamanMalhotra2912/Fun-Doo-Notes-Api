87a5997dd704f900a093d721cde99ce30c6eb822
NamanMalhotra2912 <neerajmalhotra0001@gmail.com>
2021-05-13 00:10:15 +0530

    Refacotred the nodemailer code

--- helper/validationSchema.js
+++ helper/validationSchema.js
@@ -13,10 +13,12 @@ const joi = require('@hapi/joi');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const nodemailer = require('nodemailer');
+const ejs = require('ejs');
 
-/*
-* user registration schema using hapi-joi validation.
-*/
+/**
+ *
+ * @description User ragistration schema with joi validation.
+ */
 
 const ragistationSchema = joi.object({
     firstName : joi.string().min(3).pattern(/^[A-Z][a-zA-Z]{2}/) .required(),
@@ -25,41 +27,56 @@ const ragistationSchema = joi.object({
     password : joi.string().pattern(/^[A-Z][a-zA-Z0-9]{5,}[$&^!@#()|,;:<>?/%-+][0-9]{3,}/).required()
 });
 
-/*
-* genrating token using jwt (jsonwebtoken).
-*/
+/**
+ *
+ * @description Using jsonwebtoken to genrate token.
+ */
 const createToken = (result) => {
-    const token = jwt.sign({ name: result.email }, process.env.JWT, { expiresIn: '1 hour' });
+    const token = jwt.sign({ name: result.email }, process.env.JWT, { expiresIn: '1 day' });
     console.log(token);
     return token;
 }
 
-/*
-* defining nodemailer to send the mail for forget password.
-*/
+/**
+ *
+ * @description Using nodemailer to send reset password mail.
+ */
 
 const mail = (data) => {
-const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      user: process.env.USER,
-      pass: process.env.PASS,
-    },
-  });
+  const transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: process.env.USER,
+        pass: process.env.PASS,
+      },
+    });
   
-  var mailOptions = {
-    from: 'nmalhotra1289@gmail.com',
-    to: 'nmalhotra1289@gmail.com',
-    subject: 'Sending Email using Node.js for reset password',
-    text: `Hi Raj, You can follow below shared link to reset your password.`,
-    html: '<h1>Hi Raj</h1> <p>Please follow the link shared below.</p>' 
-  };
-  
-  transporter.sendMail(mailOptions, function(error, info){
+  // var mailOptions = {
+  //   from: 'nmalhotra1289@gmail.com',
+  //   to: 'nmalhotra1289@gmail.com',
+  //   subject: 'Sending Email using Node.js for reset password',
+  //   text: `Hi Raj, You can follow below shared link to reset your password.`,
+  //   html: '<h1>Hi Raj</h1> <p>Please follow the link shared below.</p>' 
+  // };
+
+  ejs.renderFile('app/mail/mail.ejs', (error, info) => {
     if (error) {
-      console.log("this is the error from mailer "+ error);
+      console.log('error', error);
     } else {
-      console.log('Password Reset mail sent successfully, please check your mail.' + info.response);
+      const mailOption = {
+        from: 'nmalhotra1289@gmail.com',
+        to: data.email,
+        subject: 'Reset password',
+        html: `${info}<button><a href="${'http://localhost:3000/forgetPassword/'}${createToken(data)}">Button</a>
+        </button>`,
+      };
+    transporter.sendMail(mailOption, function(error, info){
+      if (error) {
+        console.log("this is the error from mailer "+ error);
+      } else {
+        console.log('Password Reset mail sent successfully, please check your mail.' + info.response);
+        }
+      });
     }
   });
 }
