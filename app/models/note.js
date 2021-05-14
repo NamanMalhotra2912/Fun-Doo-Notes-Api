5ea82997956e4088d324f14493730dc277f7baa5
NamanMalhotra2912 <neerajmalhotra0001@gmail.com>
2021-05-15 01:06:57 +0530

    feat - A new feature like added

---
+++ app/models/note.js
@@ -0,0 +1,90 @@
+/** ***********************************************************************
+ * Execution        : 1. default node       cmd> nodemon server.js
+ *
+ * Purpose          : To Create Database Schema For API
+ * 
+ * @file            : note.js
+ * @author          : Neeraj Malhotra
+ * @version         : 1.0
+ ************************************************************************* */
+ const mongoose = require('mongoose');
+ const note = require('../services/note');
+ 
+ const noteSchema = mongoose.Schema({
+   title: { type: String, required: true },
+   description: { type: String, required: true },
+   isPinned: { type: Boolean, default: false },
+   isArchived: { type: Boolean, default: false },
+   isReminder: { type: String, required: false },
+   isTrashed: { type: Boolean, default: false },
+   userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+ }, {
+   timestamps: true,
+   versionKey: false,
+ });
+ 
+ const noteModel = mongoose.model('Note', noteSchema);
+ 
+ class NoteModel {
+   createNote = (noteInfo, callback) => {
+     const note = new noteModel({
+       title: noteInfo.title,
+       description: noteInfo.description,
+       userId: noteInfo.userId
+     });
+     note.save(callback);
+   };
+ 
+   updateNote = (notesID, callback) => {
+     noteModel.findByIdAndUpdate(notesID.noteId, {
+       title: notesID.title,
+       description: notesID.description,
+     }, {new: true}).then((noteone) => {
+         callback(null, noteone);
+     }).catch((err) => {
+         callback(err, null);
+     });   
+   };
+ 
+   getNote = (callback) => {
+       noteModel.find((err, notedata) => {
+         if(err){
+             callback(err ,null);
+           }else {
+             callback(null ,notedata);
+           }
+       });     
+   };
+ 
+   getNoteById = (Ids, callback) => {
+     noteModel.findById(Ids, (err, noteresult) => {
+       if(err){
+           callback(err, null);
+       } else {
+           callback(null, noteresult);
+       }
+     });
+   };
+ 
+   deleteNote = (noteIds, callback) => {
+       noteModel.findByIdAndRemove(noteIds, (err, noteresult) => {
+           if(err){
+               callback(err, null);
+          }else {
+              callback(null, noteresult);
+          }
+       });
+   };
+ 
+   trashNote = (data, callback) => {
+       noteModel.findByIdAndUpdate(data, {isTrashed: true}, {new: true})
+       .then((note) => {
+           callback(null, note);
+       }).catch((err) => {
+           callback(err, null);
+       });
+   };
+ 
+ }
+ 
+ module.exports = new NoteModel();
