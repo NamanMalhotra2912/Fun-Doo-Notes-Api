07c9ff8e8e333d58e206bbeb1b7ae8ac9199d977
NamanMalhotra2912 <neerajmalhotra0001@gmail.com>
2021-05-31 09:19:09 +0530

    refactor - refactored the code

--- app/routes/user.js
+++ app/routes/user.js
@@ -29,7 +29,7 @@
 
         app.put('/notes/:noteId', verifyToken, note.updateNote);
 
-        app.get('/notes',verifyToken,redisMiddleWare,  note.retrieveNote);
+        app.get('/notes',verifyToken, note.retrieveNote);
 
         app.delete('/notes/:noteId', verifyToken, note.deleteNote);
 
