7c0a5f7042950559a6d59211affff9a898952203
NamanMalhotra2912 <neerajmalhotra0001@gmail.com>
2021-06-02 20:20:52 +0530

    refactor - refactored the code to make some corrections

--- app/services/user.js
+++ app/services/user.js
@@ -12,29 +12,29 @@
 
 const userRegistrationModel = require('../models/user.js');
 const bcrypt = require('bcrypt');
-const help = require ('../../helper/validationSchema.js');
-    /**
-     * 
-     * @description Creating service file for all api of register, login, forget password and reset password
-     */
-    class userData{
-        createUser = (userData, callback) =>{
-        userRegistrationModel.createUser(userData,callback);
-        }
-    
-        createLogin = (loginData, callback) => {
-        userRegistrationModel.createLogin(loginData ,(_err, result) => {
+const help = require('../../helper/validationSchema.js');
+/**
+ * 
+ * @description Creating service file for all api of register, login, forget password and reset password
+ */
+class userData {
+    createUser = (userData, callback) => {
+        userRegistrationModel.createUser(userData, callback);
+    }
+
+    createLogin = (loginData, callback) => {
+        userRegistrationModel.createLogin(loginData, (_err, result) => {
             if (result) {
                 bcrypt.compare(loginData.password, result.password, (err, data) => {
                     if (err) {
-                        callback(err ,null);
+                        callback(err, null);
                     }
                     if (data) {
                         const resultSuccess = {
                             // message: 'Successfully logeed in',
-                            data : result
+                            data: result
                         };
-                        callback(null ,resultSuccess)
+                        callback(null, resultSuccess)
 
                     } else {
                         callback('Please enter correct password')
@@ -47,26 +47,26 @@ const help = require ('../../helper/validationSchema.js');
         });
     }
 
-        forgetPassword = (data, callback) => {
+    forgetPassword = (data, callback) => {
         // console.log(data);
         userRegistrationModel.forgetPassword(data, (err, result) => {
-            if(result){
-                if(err){
-                    callback(err,null);
-                }  
-                else{
-                    callback(null ,help.mail(data));
+            if (result) {
+                if (err) {
+                    callback(err, null);
+                }
+                else {
+                    callback(null, help.mail(data));
                 }
             }
-            else{
+            else {
                 callback('Please check your email id again.');
             }
         });
     };
 
-    
-        resetPassword = (data ,callback) => {
-        userRegistrationModel.resetPassword(data ,callback);
+
+    resetPassword = (data, callback) => {
+        userRegistrationModel.resetPassword(data, callback);
     }
 }
 module.exports = new userData();
