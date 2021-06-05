1986f3463afd02d90195d0f65906dee5d9be176c
NamanMalhotra2912 <neerajmalhotra0001@gmail.com>
2021-06-05 08:53:36 +0530

    feat : added add lable to note

--- app/services/note.js
+++ app/services/note.js
@@ -7,34 +7,38 @@
  * @author          : Neeraj Malhotra
  * @version         : 1.0
  ************************************************************************* */
- const note = require('../models/note');
- const notemodel = require('../models/note');
- const {redisFunction} = require('../../helper/validationSchema.js');
- /**
- * @class NoteService to include all notes api.
- * @description create service file to apply business logic for api.
- */ 
+const note = require('../models/note');
+const notemodel = require('../models/note');
+const { redisFunction } = require('../../helper/validationSchema.js');
+/**
+* @class NoteService to include all notes api.
+* @description create service file to apply business logic for api.
+*/
 class NoteService {
     createNote = (noteInfo, callback) => {
         notemodel.createNote(noteInfo, callback);
     };
-        
+
     updateNote = (noteData, callback) => {
         const KEY = 'note';
-        notemodel.updateNote(noteData, (err,result) =>{
-            err ? callback(err, null) : redisFunction(KEY, result), callback(null, result); 
+        notemodel.updateNote(noteData, (err, result) => {
+            err ? callback(err, null) : redisFunction(KEY, result), callback(null, result);
         });
     };
-    
+
     retrieveNote = (callback) => {
         notemodel.retrieveNote((err, result) => {
-            err ? callback(err, null) : callback(null, result); 
+            err ? callback(err, null) : callback(null, result);
         });
     }
-    
+
     deleteNote = (noteIds, callback) => {
         notemodel.deleteNote(noteIds, callback);
     };
+
+    addLabelToNote = (data, callback) => {
+        notemodel.addLabelToNote(data, callback);
+    };
 }
-  
+
 module.exports = new NoteService();
