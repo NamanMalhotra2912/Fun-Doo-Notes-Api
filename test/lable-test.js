52e5f7153c09f8c4a64ba5f63510cc56e2631931
NamanMalhotra2912 <neerajmalhotra0001@gmail.com>
2021-06-11 09:26:32 +0530

    feat : added test case for addLabelTo Note

--- test/lable-test.js
+++ test/lable-test.js
@@ -94,5 +94,61 @@ describe('deleteLabel', () => {
                 res.should.have.status(401);
             });
     });
+});
 
+describe('addLabelToNote', () => {
+    it.only('givenLabelAndNoteId_whenProper_shouldAddLabelToTheNote', () => {
+        chai.request(server).post('/addLabelToNote').set('token', `${labelData.labels.genratedToken.token}`)
+            .send(labelData.labels.addLabelWithProperDetails)
+            .end((err, res) => {
+                res.should.have.status(200);
+            });
+    });
+    it.only('givenLabelId_whenImProper_ShouldNotDeleteLabel', () => {
+        chai.request(server).delete('/label/60c2cb2932aa660f550')
+            .set('token', `${labelData.labels.genratedToken.token}`).send()
+            .end((err, res) => {
+                res.should.have.status(400);
+            });
+    });
+    it.only('givenToken_whenImProper_shouldNotAddLabelToNote', () => {
+        chai.request(server).post('/addLabelToNote').set('token', `${labelData.labels.genratedToken.wrongToken}`)
+            .send(labelData.labels.addLabelWithProperDetails)
+            .end((err, res) => {
+                res.should.have.status(401);
+            });
+    });
+    it.only('givenToken_whenMissing_shouldNotAddLabelToTheNote', () => {
+        chai.request(server).post('/addLabelToNote').send(labelData.labels.addLabelWithProperDetails)
+            .end((err, res) => {
+                res.should.have.status(401);
+            });
+    });
+});
+
+describe('removeLabelToNote', () => {
+    it.only('givenLabelAndNoteId_whenProper_shouldRemoveLabelFromNote', () => {
+        chai.request(server).delete('/removeLabelFromNote')
+            .set('token', `${labelData.labels.genratedToken.token}`)
+            .send(labelData.labels.addLabelWithProperDetails)
+            .end((err, res) => {
+                res.should.have.status(200);
+            });
+    });
+
+    it.only('givenToken_whenImProper_shouldNotRemoveLabelFromNote', () => {
+        chai.request(server).delete('/removeLabelFromNote')
+            .set('token', `${labelData.labels.genratedToken.wrongToken}`)
+            .send(labelData.labels.addLabelWithProperDetails)
+            .end((err, res) => {
+                res.should.have.status(401);
+            });
+    });
+
+    it.only('givenToken_whenMissing_shouldNotRemoveLabelFromNote', () => {
+        chai.request(server).delete('/removeLabelFromNote').send(labelData.labels.addLabelWithProperDetails)
+            .end((err, res) => {
+                res.should.have.status(401);
+            });
+    });
 });
