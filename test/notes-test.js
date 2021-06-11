c1a0c63f50a1d762c4bd82383a18e87c3913051e
NamanMalhotra2912 <neerajmalhotra0001@gmail.com>
2021-06-11 23:08:54 +0530

    refactor : added some refactors in test cases

--- test/notes-test.js
+++ test/notes-test.js
@@ -13,26 +13,26 @@
  * @description : writing test cases for note api
  */
 
- const chai = require('chai');
- const chaiHttp = require('chai-http');
- const server = require('../server.js');
- 
- chai.use(chaiHttp);
- const noteData = require('./notes.json');
- 
- chai.should();
- 
- describe('notes', () => {
-    it('given_Correct_Details_Should_Able_ToCreate_Note', (done) => {
-      chai.request(server).post('/notes').set('token', `${noteData.notes.genratedToken.token}`)
-        .send(noteData.notes.createNote).end((err, res) => {
-          res.should.have.status(200);
-          done();
-        });
-    });
- });
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const server = require('../server.js');
 
- describe('inCorrectNotes', () => {
+chai.use(chaiHttp);
+const noteData = require('./notes.json');
+
+chai.should();
+
+describe('notes', () => {
+  it('given_Correct_Details_Should_Able_ToCreate_Note', (done) => {
+    chai.request(server).post('/notes').set('token', `${noteData.notes.genratedToken.token}`)
+      .send(noteData.notes.createNote).end((err, res) => {
+        res.should.have.status(200);
+        done();
+      });
+  });
+});
+
+describe('inCorrectNotes', () => {
   it('given_Wrong_Details_Should_Not_Be_Able_ToCreate_Note', (done) => {
     chai.request(server).post('/notes').set('token', `${noteData.notes.genratedToken.token}`)
       .send(noteData.notes.createNoteWithWrongData).end((err, res) => {
@@ -42,25 +42,15 @@
   });
 });
 
- describe('updateNote', () => {
+describe('updateNote', () => {
   it('given_Correct_Details_Should_Be_Able_To_Update_Note', (done) => {
     chai.request(server).put('/notes/60b700f1ad79c735800ccba3').set('token', `${noteData.notes.genratedToken.token}`)
       .send(noteData.notes.updateNoteDetails).end((err, res) => {
         res.should.have.status(200);
       });
-      done();
+    done();
   });
 });
-
-// describe('inCorrectUpdateNote', () => {
-//   it('given_inCorrect_Details_Should_Not_Be_Able_To_Update_Note', (done) => {
-//     chai.request(server).put('/notes/60b5ae6a67f4ffc93b6d6').set('token', `${noteData.notes.genratedToken.token}`)
-//       .send(noteData.notes.updateNoteDetails).end((err, res) => {
-//         res.should.have.status(200);
-//       });
-//       done();
-//   });
-// });
 
 describe('retreiveNotes', () => {
   it('given_Correct_Details_should_Retrive_Note', (done) => {
