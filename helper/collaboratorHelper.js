8a1b1a2acaf00e24543303d57b2652ddd3ac9c64
NamanMalhotra2912 <neerajmalhotra0001@gmail.com>
2021-06-11 23:10:05 +0530

    feat : created ejs for adding label to note

---
+++ helper/collaboratorHelper.js
@@ -0,0 +1,30 @@
+const nodemailer = require('nodemailer');
+const ejs = require('ejs');
+
+const mail = () => {
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: process.env.USER,
+            pass: process.env.PASS,
+        },
+    });
+
+    ejs.renderFile('app/mail/addedCollaborator.ejs', (error, info) => {
+        if (error) {
+            console.log('error', error);
+        } else {
+            const mailOption = {
+                from: 'nmalhotra1289@gmail.com',
+                to: 'nmalhotra1289@gmail.com',
+                subject: 'Added Collaborator',
+                html: `${info}`,
+            };
+            transporter.sendMail(mailOption, function (error, info) {
+                (error) ? console.log("this is the error from mailer " + error) : console.log('Password Reset mail sent successfully, please check your mail.' + info.response);
+            });
+        }
+    });
+}
+
+module.exports = { mail };
