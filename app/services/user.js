35cd15f5006ed28ce32e00d1af9223a45b660f74
NamanMalhotra2912 <neerajmalhotra0001@gmail.com>
2021-06-28 21:36:39 +0530

    refactor : refactor the services and added comments

--- app/services/user.js
+++ app/services/user.js
@@ -13,15 +13,27 @@
 const userRegistrationModel = require('../models/user.js');
 const bcrypt = require('bcrypt');
 const help = require('../../helper/validationSchema.js');
+const jwt = require('jsonwebtoken');
 /**
  * 
  * @description Creating service file for all api of register, login, forget password and reset password
  */
 class UserData {
+    /**
+     * 
+     * @param {*} userData 
+     * @param {*} callback 
+     * @description : createUser will take the request from controller and pass it to models
+     */
     createUser = (userData, callback) => {
         userRegistrationModel.createUser(userData, callback);
     }
-
+    /**
+     * 
+     * @param {*} loginData 
+     * @param {*} callback 
+     * @description : login will take the request from controller for login
+     */
     login = (loginData, callback) => {
         userRegistrationModel.login(loginData, (_err, result) => {
             if (result) {
@@ -42,7 +54,12 @@ class UserData {
             }
         });
     }
-
+    /**
+     * 
+     * @param {*} data 
+     * @param {*} callback 
+     * @description : forgetPassword will take the request from controller and pass it to models
+     */
     forgetPassword = (data, callback) => {
         userRegistrationModel.forgetPassword(data, (err, result) => {
             if (result) {
@@ -53,19 +70,34 @@ class UserData {
             }
         });
     };
-
+    /**
+     * 
+     * @param {*} data 
+     * @param {*} callback 
+     * @description : resetPassword will take the request from controller and pass it to models
+     */
     resetPassword = (data, callback) => {
         userRegistrationModel.resetPassword(data, callback);
     }
-
+    /**
+     * 
+     * @param {*} socialLoginData 
+     * @param {*} callback 
+     * @description : socialLogin will take the request from controller and perform the service here
+     *                  and pass the details to models
+     */
     socialLogin = (socialLoginData, callback) => {
         userRegistrationModel.socialLogin(socialLoginData, (err, result) => {
             if (err) {
                 callback(err, null);
             }
-            if (data) {
-                callback(null, result)
-
+            if (result) {
+                let payload = {
+                    '_id': result._id,
+                    'userName': result.userName
+                };
+                let token = jwt.sign(payload, process.env.JWT);
+                callback(null, token)
             } else {
                 callback('Please check for details');
             }
