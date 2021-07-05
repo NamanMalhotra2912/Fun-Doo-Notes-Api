b9ac49b2d56ab2429a3f4f69429fbd43a393f60b
NamanMalhotra2912 <neerajmalhotra0001@gmail.com>
2021-07-05 08:44:35 +0530

    refactor : refactor the code as per the rules of eslint

--- helper/validationSchema.js
+++ helper/validationSchema.js
@@ -1,3 +1,4 @@
+/* eslint-disable indent */
 /* eslint-disable spaced-comment */
 /*************************************************************************
  * Execution        : 1. default node       cmd> npm start
@@ -67,12 +68,12 @@ const mail = (data) => {
       });
     }
   });
-}
+};
 /**
- * 
- * @param {*} req 
- * @param {*} res 
- * @param {*} next 
+ *
+ * @param {*} req
+ * @param {*} res
+ * @param {*} next
  * @description : verifyToken will validate the token generated
  */
 
@@ -86,8 +87,7 @@ const verifyToken = (req, res, next) => {
         const userId = decode.id;
       }
       next();
-    })
-
+    });
   } catch (error) {
     res.status(401).send({
       error: 'Your token has expiered',
@@ -95,33 +95,33 @@ const verifyToken = (req, res, next) => {
   }
 };
 /**
- * 
- * @param {*} KEY 
- * @param {*} value 
+ *
+ * @param {*} KEY
+ * @param {*} value
  * @description : here we have set the key and value in redis function
  */
 const redisFunction = (KEY, value) => {
   client.setex(KEY, 1200, JSON.stringify(value));
-
-}
+};
 /**
- * 
- * @param {*} req 
- * @param {*} res 
- * @param {*} next 
+ *
+ * @param {*} req
+ * @param {*} res
+ * @param {*} next
  * @description : redisMiddleWare is the middle ware of the redis.
  */
 const redisMiddleWare = (req, res, next) => {
   client.get('note', (err, note) => {
     if (err) {
-      throw err
+      throw err;
     } else if (note) {
-      res.send(JSON.parse(note))
+      res.send(JSON.parse(note));
     } else {
       next();
     }
-  })
+  });
+};
 
-}
-
-module.exports = { registationSchema, createToken, mail, verifyToken, redisFunction, redisMiddleWare };
+module.exports = {
+  registationSchema, createToken, mail, verifyToken, redisFunction, redisMiddleWare,
+};
